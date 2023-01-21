893089498dc02f93eeb1917c506f15ac95066c00
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2023-01-21 18:56:00 -0500

    Going to /read/comic auto redirects to /read/comic/1

---
+++ frontend/src/routes/read/[comic]/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { onMount } from "svelte";
+  import {goto} from "$app/navigation";
+  import {page} from "$app/stores";
+
+  onMount(async () => {
+    goto(`/read/${$page.params.comic}/1`)
+  })
+</script>
