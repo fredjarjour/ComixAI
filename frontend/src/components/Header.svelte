95bafcbd40e72f0211bf99a2ad906f7a69ec1f21
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2023-01-21 19:06:12 -0500

    Added loading for comic section

--- frontend/src/components/Header.svelte
+++ frontend/src/components/Header.svelte
@@ -1,5 +1,9 @@
+<script>
+  import { goto } from '$app/navigation';
+</script>
+
 <div class="header">
-  <h1 class="title">ComixAI</h1>
+  <h1 class="title" on:click={() => {goto('/');}}>ComixAI</h1>
 </div>
 
 <style>
@@ -12,5 +16,6 @@
   .title {
     font-size: 2rem;
     margin-bottom: 0rem;
+    cursor: pointer;
   }
 </style>
--- frontend/src/routes/read/[comic]/[section]/+page.svelte
+++ frontend/src/routes/read/[comic]/[section]/+page.svelte
@@ -2,24 +2,35 @@
   import placeholder from '$lib/images/imgplaceholder.png'
   import {page} from '$app/stores'
   // {$page.params.section} to get the value of the url query
+
+  let loadSection = async () => {
+    //let response = await fetch(`/api/read/${$page.params.comic}/${$page.params.section}`)
+    //let data = await response.json()
+    return;
+  }
 </script>
 
 <div>
-  <div class="image_and_text">
-    <img src="{placeholder}" alt=""/>
-    <p>Shrek: It's time to end this! </p>
-  </div>
+  {#await loadSection()}
+  <div aria-busy="true"></div>
+  {:then section}
+    <div class="image_and_text">
+      <img src="{placeholder}" alt=""/>
+      <p>Shrek: It's time to end this! </p>
+    </div>
 
-  <div class="image_and_text flipped">
-    <img src="{placeholder}" alt=""/>
-    <p>Mario: Here I come!</p>
-  </div>
+    <div class="image_and_text flipped">
+      <img src="{placeholder}" alt=""/>
+      <p>Mario: Here I come!</p>
+    </div>
 
-  <div class="image_and_text">
-    <img src="{placeholder}" alt=""/>
-    <p>Shrek: You won't get away that easily!</p>
-  </div>
+    <div class="image_and_text">
+      <img src="{placeholder}" alt=""/>
+      <p>Shrek: You won't get away that easily!</p>
+    </div>
+  {/await}
 </div>
+
 
 
 
